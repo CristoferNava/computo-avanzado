a3f99a9056c040aeb57edcd89a2ca378c7b0e88a
CristoferNava <cj.carmonanava@ugto.mx>
2021-12-02 22:24:53 -0600

    Mandelbrot ready to add MPI

--- mandelbrot/main.py
+++ mandelbrot/main.py
@@ -1,8 +1,9 @@
 from mandelbrot import mandelbrot_set
 
 width = int(input("Introduzca el ancho de la imagen en pixeles: "))
-height = int(input("Introuduzda el alto de la imagen en pixeles: "))
+height = int(input("Introduzca el alto de la imagen en pixeles: "))
 n_iters = int(input("Introduzca el número de iteraciones: "))
 
 print("Procesando...")
 mandelbrot_set(width, height, n_iters)
+print("Listo!")
--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -9,33 +9,27 @@ def color_period(n: int):
     return tuple(color.astype(int))
 
 
-def mandelbrot(x: int, y: int, n_iters: int):
-    """ 
-    Z_0 = 0 + 0i
-    Z_N+1 = Z_N^2 + C 
-    a cada pixel le corresponde un número complejo C
-    """
-
-    z_0 = complex(x, y)
+def mandelbrot(c: complex, n_iters: int):
     z = 0
     for i in range(1, n_iters):
         # if z diverges it leaves the circle of radius 2 in the complex plane
         # so it does not belong to the Mandelbrot set
         if abs(z) > 2:
             return color_period(i)
-        z = z * z + z_0
+        z = z * z + c
     return (0, 0, 0)  # it belongs to Mandelbrot's set
 
 
 def mandelbrot_set(width: int, height: int, n_iters: int):
     # creates the image in RGB
-    img = Image.new('RGB', (width, height))
-    pixels = img.load()
+    image = Image.new('RGB', (width, height))
+    pixels = image.load()
 
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
-            pixels[x, y] = mandelbrot(x=(x - (0.75 * width)) / (width / 4),
-                                      y=(y - (width / 4)) / (width / 4),
-                                      n_iters=n_iters)
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            # converts pixel coordinate to complex number
+            c = complex((x - (0.75 * width)) / (width / 4),
+                        (y - (width / 4)) / (width / 4))
+            pixels[x, y] = mandelbrot(c, n_iters)
 
-    img.show()
+    image.save("/Users/cristofer/Desktop/mandelbrot.png")
